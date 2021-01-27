ef7e7ada10cf002956c32999f2f7ee851ca48f43
timishk-glitch <rohit.kumar@anchanto.com>
2021-01-27 10:57:31 +0530

    my_profile function added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#home'
+  get 'my_profile', to: 'users#my_profile'
+  resources:users, only: [:index, :show]
   resources:posts, only: [:new, :create, :index, :show ,:edit ,:update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
